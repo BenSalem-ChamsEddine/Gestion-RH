fcaab10753b8e9682b1b8334dae8b2945e7122be
chams eddine ben salem <bensalemchams@gmail.com>
2017-09-07 15:55:37 +0200

    change postgres address and other

--- Config-Service/src/main/java/de/cynapsys/ConfigService/ConfigServiceApplication.java
+++ Config-Service/src/main/java/de/cynapsys/ConfigService/ConfigServiceApplication.java
@@ -3,7 +3,9 @@ package de.cynapsys.ConfigService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
+@RefreshScope
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServiceApplication {
